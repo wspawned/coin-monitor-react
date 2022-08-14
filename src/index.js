6d0bc4c80a25101987ad5071df696a152a2b1866
wspawned <wspawned@gmail.com>
2022-08-15 01:24:14 +0300

    browser router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Monitor from './Monitor';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={ <Monitor /> } />
+                
+            </Routes>
+        </BrowserRouter>
+
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Monitor />
+    <App />
 );
